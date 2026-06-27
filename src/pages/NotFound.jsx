2e4b407dbe7c865385f800593749a7667aaf7941
ardhii-m <ardhiim008@gmail.com>
2026-06-27 10:56:54 +0700

    style(not-found): updated style for 404 page.

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-
 function NotFound() {
   return (
-    <section>
-      <h2>404</h2>
-      <p>Page Not Found.</p>
+    <section className="flex justify-center px-2 pt-8 pb-6 transition-colors duration-200">
+      <div className="flex flex-col items-center justify-center w-full max-w-sm text-center bg-[#f0f0f0] shadow-2xl rounded-2xl space-y-4 p-4 mx-4">
+      <h2 className="text-4xl font-bold">404</h2>
+      <p className="text-lg">Page Not Found.</p>
+      </div>
     </section>
   );
 }
